2319dd81560c12333f314c8470ef7a8e48495544
Sunryeo <elma9700@gmail.com>
2021-09-16 14:41:38 +0900

    Feat: sign-up

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,11 +1,12 @@
-
 const { Router } = require("express");
 const { sign } = require("./sign");
-const { getToken, getUserInfo } = require("./sign/google");
-
 const router = Router();
+const { getToken, getUserInfo } = require("./sign/google");
+const { generalSignUp, authorSignUp } = require("./sign/signUp");
 
 router.post("/receive/token", getToken);
 router.get("/receive/userinfo?", getUserInfo);
+router.post("/sign-up/user", generalSignUp);
+router.post("/sign-up/author", authorSignUp);
 
 module.exports = router;
