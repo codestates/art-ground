1e2945e1526bd9ce16e587490192cb18aa402f8c
devjade <jiyoung.park98@gmail.com>
2021-09-16 18:10:18 +0900

    error/navbar

--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -1,85 +1,69 @@
-import { useState } from 'react';
-import { Link, useHistory } from 'react-router-dom';
-import styles from './Navbar.module.css';
+import { Link, useHistory } from 'react-router-dom'
+
+import styles from './Navbar.module.css'
 
 const Navbar = ({ isLogin, handleLogout }) => {
-  
-  const [navOpen, setNavOpen] = useState(false);
-  
-  const history = useHistory();
+  const history = useHistory()
 
   const clickLogo = () => {
-    history.push('/');
-  };
-  
-  const handleNavClose = () => {
-    setNavOpen(false)
-  }
-  const handleNavOpen = () => {
-    setNavOpen(true)
+    history.push('/')
   }
 
   return (
     <section className={styles.container}>
       <div className={styles.navBox}>
-        <div className={styles.topNavBox}>
-          <span className={styles.moreOpt} onClick={handleNavOpen}><i class="fas fa-bars"></i></span>
-          <ul className={styles.btns}>
-            {!isLogin
-              ? (
-                <li
-                  className={styles.btn}
-                  onClick={() => {
-                    history.push('/signin');
-                  }}
-                >
-                  로그인
-                </li>
-                )
-              : (
-                <li
-                  className={styles.btn}
-                  onClick={() => {
-                    handleLogout();
-                    history.push('/');
-                  }}
-                >
-                  로그아웃
-                </li>
-                )}
-            <Link to='/join'>
-              <li className={styles.btn}>회원가입</li>
-            </Link>
-            <Link to='/mypage'>
-              <li className={styles.btn}>마이페이지</li>
-            </Link>
-          </ul>
-        </div>
+        <ul className={styles.btns}>
+          {!isLogin ? (
+            <li
+              className={styles.btn}
+              onClick={() => {
+                history.push('/signin')
+              }}
+            >
+              로그인
+            </li>
+          ) : (
+            <li
+              className={styles.btn}
+              onClick={() => {
+                handleLogout()
+                history.push('/')
+              }}
+            >
+              로그아웃
+            </li>
+          )}
+          <Link to='/join'>
+            <li className={styles.btn}>회원가입</li>
+          </Link>
+          <Link to='/mypage'>
+            <li className={styles.btn}>마이페이지</li>
+          </Link>
+        </ul>
         <div className={styles.logo}>
-          <img className={styles.logoImg} src="../../../images/Original on Transparent.png" alt="logo" onClick={clickLogo} />
+          <span className={styles.logoborder} onClick={clickLogo}>
+            ART-GROUND
+            {/* <img src="" alt="" className={styles.img}></img> */}
+          </span>
         </div>
-        <div className={navOpen? styles.category: styles.categoryClose}>
+        <div className={styles.category}>
           <ul className={styles.categoryBox}>
-            <li className={styles.title} onClick={handleNavClose}>ABOUT</li>
+            <li className={styles.title}>ABOUT</li>
             <Link to='/gallery'>
-              <li className={styles.title} onClick={handleNavClose}>GALLERY</li>
+              <li className={styles.title}>GALLERY</li>
             </Link>
             <Link to='/reviewlist'>
-              <li className={styles.title} onClick={handleNavClose}>REVIEW</li>
+              <li className={styles.title}>REVIEW</li>
             </Link>
-            <Link to='/register'>
-              <li className={styles.title} onClick={handleNavClose}>REGISTER</li>
+            <li className={styles.title}>EXHIBITION??</li>
+            <Link to='/contact'>
+              <li className={styles.title}>CONTACT</li>
             </Link>
-            <Link to='/auction'>
-              <li className={styles.title} onClick={handleNavClose}>AUCTION</li>
-            </Link>
-            <li className={styles.title} onClick={handleNavClose}>CONTACT</li>
           </ul>
-          <span className={styles.closeBtn} onClick={handleNavClose}><i class="fas fa-times"></i></span>
         </div>
       </div>
     </section>
-  );
-};
+  )
+}
 
-export default Navbar;
+export default Navbar
