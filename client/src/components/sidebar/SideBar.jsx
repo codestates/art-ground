d234c981fb0f72528bf66486d7c3d7359b8e4544
tkdkagody <tkdkagody@gmail.com>
2021-09-17 04:39:01 +0900

    layout/signup

--- client/src/components/sidebar/SideBar.jsx
+++ client/src/components/sidebar/SideBar.jsx
@@ -5,10 +5,6 @@ import React from "react";
 const SideBar = () => {
   return (
     <section className={styles.container}>
-      <div className={styles.category}>내 정보보기</div>
-      <div className={styles.category}>찜한 전시회</div>
-      <div className={styles.category}>내 전시회</div>
-      <div className={styles.category}>내가 참여한 경매</div>
       준비중입니다 페이지로 바뀔예정..
     </section>
   );
