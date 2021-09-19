39b1ea31eac98cbbaa7dde9ff5f66d2ad997b20c
lee <xehddnr@naver.com>
2021-09-20 05:36:42 +0900

    feature/register

--- server/controllers/exhibition/register.js
+++ server/controllers/exhibition/register.js
@@ -5,8 +5,8 @@ const {
 } = require("../../models");
 const { isValid } = require("../../utils/validCheck");
 module.exports.register = async (req, res) => {
-  //const userInfo = isAuthorized(req);
-  const userInfo = { id: 3 };
+  const userInfo = isAuthorized(req);
+
   if (userInfo) {
     const { id: author_id } = userInfo;
     const {
