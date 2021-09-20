f8e2055c0cdf73a049b9e43ba22647641afb66c5
Sunryeo <elma9700@gmail.com>
2021-09-20 19:42:29 +0900

    Fix: signUp.js-author

--- server/controllers/sign/signUp.js
+++ server/controllers/sign/signUp.js
@@ -60,45 +60,45 @@ module.exports = {
   // 작가 회원가입
   authorSignUp: async (req, res) => {
     try {
-      const { authorEmail, password, name, authorDesc, userType } = req.body;
-      console.log(authorEmail, password, name, authorDesc, userType);
+      const { authorEmail, password, name, userType } = req.body;
+      console.log(authorEmail, password, name, userType);
 
-      if (!authorEmail || !password || !name || !userType || !authorDesc) {
+      if (!authorEmail || !password || !name || !userType) {
         return res
           .status(422)
           .json({ message: "insufficient parameters supplied" });
       }
       // password 암호화 작업
       // cryptojs 복호화
-      let byte = cryptoJS.AES.decrypt(password, process.env.CRYPTOJS_SECRETKEY);
+      let byte = CryptoJS.AES.decrypt(password, process.env.CRYPTOJS_SECRETKEY);
       console.log("byte:", byte);
-      password = JSON.parse(byte.toString(cryptoJS.enc.Utf8)).password;
-      console.log("password:", password);
+      const decryptedPassword = JSON.parse(byte.toString(CryptoJS.enc.Utf8));
+      console.log("password:", decryptedPassword);
       // bcrypt 재암호화
-      const salt = bcrypt.genSalt(saltRounds);
-      password = bcrypt.hash(password, salt);
-
+      const salt = await bcrypt.genSalt(saltRounds);
+      console.log("salt:", salt);
+      const encryptedPassword = await bcrypt.hash(decryptedPassword, salt);
+      console.log("encrypted:", encryptedPassword);
       // time stamp 시간이 한국 시간이 아닌듯..?
       // async await으로 리팩토링 진행 중
-      users
-        .findOrCreate({
-          where: {
-            user_email: authorEmail,
-          },
-          default: {
-            password: password,
-            nickname: name,
-            user_type: userType,
-          },
-        })
-        .then(([result, created]) => {
-          if (!created) {
-            return res.status(409).json({ message: "email exists" });
-          }
-          const data = result.dataValues;
-          console.log("userinfo:", data);
-          return res.status(201).json({ message: "sign-up ok" });
+      const data = await users.findOne({
+        where: {
+          user_email: authorEmail,
+        },
+      });
+      console.log("data:", data);
+      if (data) {
+        return res.status(409).json({ message: "email exists" });
+      } else {
+        const info = await users.create({
+          user_email: authorEmail,
+          password: encryptedPassword,
+          nickname: name,
+          user_type: userType,
         });
+        console.log("info:", info);
+        return res.status(201).json({ message: "sign-up ok" });
+      }
     } catch (error) {
       console.log(error);
     }
