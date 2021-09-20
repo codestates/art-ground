d270b22ef7e9321d3cfe0ff7e552455e21f7d399
Sunryeo <elma9700@gmail.com>
2021-09-20 19:24:50 +0900

    Fix: signIn.js

--- server/controllers/sign/signIn.js
+++ server/controllers/sign/signIn.js
@@ -1,11 +1,11 @@
 require("dotenv").config();
 const { users } = require("../../models");
 const { generateAccessToken, sendAccessToken } = require("../tokenFunction");
-const cryptoJS = require("crypto-js");
+const CryptoJS = require("crypto-js");
 const bcrypt = require("bcrypt");
 
 module.exports = {
-  signIn: (req, res) => {
+  signIn: async (req, res) => {
     const { userEmail, password } = req.body;
     console.log(userEmail, password);
     try {
@@ -19,25 +19,26 @@ module.exports = {
           if (!data) {
             return res.status(401).json({ message: "invalid user" }); //404로 바꿔야 하나?
           }
-          let byte = cryptoJS.AES.decrypt(
+          let byte = CryptoJS.AES.decrypt(
             password,
             process.env.CRYPTOJS_SECRETKEY
           );
           console.log("byte:", byte);
 
-          let decodedPassword = JSON.parse(byte.toString(cryptoJS.enc.Utf8));
+          let decodedPassword = await JSON.parse(
+            byte.toString(CryptoJS.enc.Utf8)
+          );
           console.log("decoded:", decodedPassword);
-
+          // console.log("data:", data.dataValues);
           const validPassword = await bcrypt.compare(
-            decodedPassword.password,
+            decodedPassword,
             data.dataValues.password
           );
           console.log("validpw:", validPassword);
 
           if (validPassword) {
             delete data.dataValues.password;
-            delete data.dataValues.iat;
-            // delete data.dataValues.exp;
+            console.log("data:", data.dataValues);
             const accessToken = generateAccessToken(data.dataValues);
             sendAccessToken(res, accessToken);
           } else {
