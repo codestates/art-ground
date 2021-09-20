438867cb5e132caa7cdf1d89c0e40ab09d2119f4
Sunryeo <elma9700@gmail.com>
2021-09-20 22:08:53 +0900

    Refactor: controllers

--- server/controllers/sign/index.js
+++ server/controllers/sign/index.js
@@ -1,1 +1,20 @@
-const { google } = require("./google");
+const { Router } = require("express");
+const router = Router();
+const googleCtrl = require("./google");
+const signCtrl = require("./sign.ctrl");
+
+// 일반 회원가입
+router.post("/sign-up/user", signCtrl.generalSignUp);
+
+// 작가 회원가입
+router.post("/sign-up/author", signCtrl.authorSignUp);
+
+// 로그인
+router.post("/sign-in", signCtrl.signIn);
+
+// 로그아웃
+router.post("/sign-out", signCtrl.signOut);
+
+// Goole
+router.get("/receive/userinfo?", googleCtrl.getUserInfo);
+router.post("/receive/token", googleCtrl.getToken);
