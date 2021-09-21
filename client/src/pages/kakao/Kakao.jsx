6ef4177a47127f873dd7c654139642aedc23bb38
tkdkagody <tkdkagody@gmail.com>
2021-09-22 04:58:24 +0900

    modify import err

--- client/src/pages/kakao/Kakao.jsx
+++ client/src/pages/kakao/Kakao.jsx
@@ -1,7 +1,8 @@
-import styles from './Kakao.module.css'
+import styles from "./Kakao.module.css";
+
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 require("dotenv").config();
-import React, { useState, useEffect } from 'react'
-import axios from 'axios';
 
 const Kakao = (props) => {
   const [accessToken, setAccessToken] = useState();
@@ -23,16 +24,18 @@ const Kakao = (props) => {
     getUserInfo(accessToken);
     return () => {};
   }, [accessToken]);
-  
+
   const getAccessToken = (authorizationCode) => {
     axios
-      .post(`${process.env.LOCAL_SERVER_URI}/kakao-login/token`, { authorizationCode })
+      .post(`${process.env.LOCAL_SERVER_URI}/kakao-login/token`, {
+        authorizationCode,
+      })
       .then((res) => {
         console.log(res.data);
         if (!refreshToken) {
           setRefreshToken(res.data.refresh_token);
         }
-        setAccessToken(res.data.access_token)
+        setAccessToken(res.data.access_token);
       })
       .catch((error) => {
         console.log(error);
@@ -41,7 +44,9 @@ const Kakao = (props) => {
 
   const getUserInfo = (accessToken) => {
     axios
-      .get(`${process.env.LOCAL_SERVER_URI}/kakao-login/userinfo?accessToken=${accessToken}`)
+      .get(
+        `${process.env.LOCAL_SERVER_URI}/kakao-login/userinfo?accessToken=${accessToken}`
+      )
       .then((res) => {
         console.log(res.data);
         setUserInfo(res.data);
@@ -55,7 +60,7 @@ const Kakao = (props) => {
     <section className={styles.container}>
       <div>카카오</div>
     </section>
-  )
-}
+  );
+};
 
-export default Kakao
+export default Kakao;
