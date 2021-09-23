f131b2080d80c69ec75e8fa3b052558fab6e11a3
Sunryeo <elma9700@gmail.com>
2021-09-23 18:00:52 +0900

    Feat: approveExhibition

---
+++ server/controllers/admin/admin.ctrl.js
@@ -0,0 +1,51 @@
+const { exhibition } = require("../../models");
+const { isAuthorized } = require("../../utils/tokenFunction");
+
+module.exports = {
+  approveExhibition: (req, res) => {
+    const userInfo = isAuthorized(req);
+    const { postId } = req.body;
+    console.log("userInfo:", userInfo, postId);
+
+    if (userInfo.dataValues.user_type === 3) {
+      exhibition
+        .findOne({
+          where: {
+            id: postId,
+          },
+        })
+        .then((data) => {
+          console.log("data:".data);
+          if (!data) {
+            res.status(404).json({
+              message: "exhibition not exist",
+            });
+          } else {
+            exhibition.update(
+              {
+                status: 3,
+              },
+              {
+                where: {
+                  id: postId,
+                },
+              }
+            );
+          }
+        })
+        .then((result) => {
+          console.log("result:", result);
+          res.status(200).json({
+            message: "exhibition successfully approved",
+          });
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    } else {
+      res.status(401).json({
+        message: "invalid access token",
+      });
+    }
+  },
+};
