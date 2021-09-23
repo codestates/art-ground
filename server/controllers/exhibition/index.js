cae99c182fe950fd70eecd23dbfa871bc3e43228
lee <xehddnr@naver.com>
2021-09-23 19:17:45 +0900

    feature/getExhibition

--- server/controllers/exhibition/index.js
+++ server/controllers/exhibition/index.js
@@ -7,27 +7,51 @@ const {
 
 module.exports.getExhibition = async (req, res) => {
   const { type: exhibit_type } = req.params;
-  const result = await exhibitionModel.findAll({
-    include: [
-      {
-        model: imagesModel,
-        as: "images",
-      },
-      {
-        model: likeModel,
-        as: "likes",
-      },
-      {
-        attributes: ["user_email", "nickname", "profile_img", "author_desc"],
-        model: userModel,
-        as: "author",
-      },
-    ],
-    where: {
-      exhibit_type,
-    },
-  });
-  const data = result.map((el) => el.dataValues);
 
-  res.status(200).json({ data });
+  if (!exhibit_type) {
+    const result = await exhibitionModel.findAll({
+      include: [
+        {
+          model: imagesModel,
+          as: "images",
+        },
+        {
+          model: likeModel,
+          as: "likes",
+        },
+        {
+          attributes: ["user_email", "nickname", "profile_img", "author_desc"],
+          model: userModel,
+          as: "author",
+        },
+      ],
+    });
+    const data = result.map((el) => el.dataValues);
+
+    res.status(200).json({ data });
+  } else {
+    const result = await exhibitionModel.findAll({
+      include: [
+        {
+          model: imagesModel,
+          as: "images",
+        },
+        {
+          model: likeModel,
+          as: "likes",
+        },
+        {
+          attributes: ["user_email", "nickname", "profile_img", "author_desc"],
+          model: userModel,
+          as: "author",
+        },
+      ],
+      where: {
+        exhibit_type,
+      },
+    });
+    const data = result.map((el) => el.dataValues);
+
+    res.status(200).json({ data });
+  }
 };
