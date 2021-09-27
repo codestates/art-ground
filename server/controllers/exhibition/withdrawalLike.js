5a4fbdf0e167a3368ea8b6aa010a246230a5b4ad
lee <xehddnr@naver.com>
2021-09-27 21:30:51 +0900

    modify withdrawl

--- server/controllers/exhibition/withdrawalLike.js
+++ server/controllers/exhibition/withdrawalLike.js
@@ -1,12 +1,16 @@
 const { likes } = require("../../models");
-
+const { Op } = require("sequelize");
 module.exports.withdrawalLike = async (req, res) => {
   const userInfo = isAuthorized(req);
   if (userInfo) {
     const { postId: exhibition_id } = req.params;
     const { id: user_id } = userInfo;
 
-    const result = await likes.destroy({ where: { exhibition_id, user_id } });
+    const result = await likes.destroy({
+      where: {
+        [Op.and]: [{ exhibition_id }, { user_id }],
+      },
+    });
 
     if (result) {
       res.status(200).json({
