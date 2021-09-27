d4b7babe317ed488f2ee841bbe5a5572cbf3a922
lee <xehddnr@naver.com>
2021-09-27 21:52:10 +0900

    test withdrawalLike

--- server/controllers/exhibition/withdrawalLike.js
+++ server/controllers/exhibition/withdrawalLike.js
@@ -5,21 +5,22 @@ module.exports.withdrawalLike = async (req, res) => {
   if (userInfo) {
     const { postId: exhibition_id } = req.params;
     const { id: user_id } = userInfo;
+    res.status(200).json({ message: "ok" });
+    // const result = await likes.destroy({  where: {
+    //   [Op.and]: [
+    //     { exhibition_id },
+    //     { user_id }
+    //   ]
+    // }});
 
-    const result = await likes.destroy({
-      where: {
-        [Op.and]: [{ exhibition_id }, { user_id }],
-      },
-    });
-
-    if (result) {
-      res.status(200).json({
-        message: "successfully delete like",
-      });
-    }
-  } else {
-    res.status(401).json({
-      message: "invalid user",
-    });
+    //   if (result) {
+    //     res.status(200).json({
+    //       message: "successfully delete like",
+    //     });
+    //   }
+    // } else {
+    //   res.status(401).json({
+    //     message: "invalid user",
+    //   });
   }
 };
