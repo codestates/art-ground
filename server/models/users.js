a8639a89f34b8d2ff44a6ee7fd385cce1adc1467
lee <xehddnr@naver.com>
2021-09-28 03:08:46 +0900

    modify model/users

--- server/models/users.js
+++ server/models/users.js
@@ -37,6 +37,10 @@ module.exports = function (sequelize, DataTypes) {
         type: DataTypes.STRING(255),
         allowNull: true,
       },
+      login_type: {
+        type: DataTypes.INTEGER,
+        allowNull: true,
+      },
     },
     {
       sequelize,
