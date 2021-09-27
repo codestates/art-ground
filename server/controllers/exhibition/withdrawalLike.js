cbe6eb92b073e4da2f034d78792c260a851bec1c
lee <xehddnr@naver.com>
2021-09-27 21:58:29 +0900

    modify withdrawalLike

--- server/controllers/exhibition/withdrawalLike.js
+++ server/controllers/exhibition/withdrawalLike.js
@@ -1,26 +1,26 @@
 const { likes } = require("../../models");
 const { Op } = require("sequelize");
+const { isAuthorized } = require("../../utils/tokenFunction");
 module.exports.withdrawalLike = async (req, res) => {
   const userInfo = isAuthorized(req);
   if (userInfo) {
     const { postId: exhibition_id } = req.params;
     const { id: user_id } = userInfo;
-    res.status(200).json({ message: "ok" });
-    // const result = await likes.destroy({  where: {
-    //   [Op.and]: [
-    //     { exhibition_id },
-    //     { user_id }
-    //   ]
-    // }});
 
-    //   if (result) {
-    //     res.status(200).json({
-    //       message: "successfully delete like",
-    //     });
-    //   }
-    // } else {
-    //   res.status(401).json({
-    //     message: "invalid user",
-    //   });
+    const result = await likes.destroy({
+      where: {
+        [Op.and]: [{ exhibition_id }, { user_id }],
+      },
+    });
+
+    if (result) {
+      res.status(200).json({
+        message: "successfully delete like",
+      });
+    }
+  } else {
+    res.status(401).json({
+      message: "invalid user",
+    });
   }
 };
