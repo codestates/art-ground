0daf3240a4e9fdc8ec273cb0486b1de52a2828a2
Sunryeo <elma9700@gmail.com>
2021-09-30 22:44:33 +0900

    Refactor: getMyLikes

--- server/controllers/mypage/likes.js
+++ server/controllers/mypage/likes.js
@@ -18,55 +18,104 @@ const { likes, exhibition, images, users } = require("../../models");
 const { isAuthorized } = require("../../utils/tokenFunction");
 module.exports.getMyLikes = async (req, res) => {
   const userInfo = isAuthorized(req);
+  console.log("userInfo:", userInfo);
+  try {
+    if (!userInfo) {
+      res.status(401).json({ message: "invalid access token" });
+    } else {
+      const { id: user_id } = userInfo;
+      // likes
+      let likesData = await likes.findAll({
+        attributes: ["exhibition_id"],
+        where: {
+          user_id,
+        },
+      });
 
-  const { id: user_id } = userInfo;
+      let exhibition_ids = likesData.map((el) => el.dataValues.exhibition_id);
+      exhibition_ids = exhibition_ids.sort((a, b) => a - b);
 
-  // likes
-  const result = await likes.findAll({
-    attributes: ["exhibition_id"],
-    where: {
-      user_id,
-    },
-  });
+      // exhibition
+      const myExhibition = await exhibition.findAll({
+        where: {
+          id: exhibition_ids,
+        },
+      });
 
-  const exhibition_ids = result.map((el) => el.dataValues.exhibition_id);
+      let author_ids = myExhibition.map((el) => el.dataValues.author_id);
+      author_ids = author_ids.sort((a, b) => a - b);
 
-  // exhibition
-  const myExhibition = await exhibition.findAll({
-    where: {
-      id: exhibition_ids,
-    },
-  });
+      // author
+      const authors = [];
+      for (let h = 0; h < author_ids.length; h++) {
+        const userData = await users.findOne({
+          where: {
+            id: author_ids[h],
+          },
+        });
+        authors.push(userData);
+      }
 
-  const author_ids = myExhibition.map((el) => el.dataValues.author_id);
+      // images
+      const myExhibitionImgs = await images.findAll({
+        where: {
+          exhibition_id: exhibition_ids,
+        },
+      });
 
-  // author
-  let authors = await users.findAll({
-    where: {
-      id: author_ids,
-    },
-  });
+      // exhibition x users
+      for (let l = 0; l < myExhibition.length; l++) {
+        for (let m = 0; m < authors.length; m++) {
+          const myExhibitEl = myExhibition[l].dataValues;
+          if (myExhibitEl.author_id === authors[m].dataValues.id) {
+            const { user_email, nickname } = authors[m].dataValues;
+            myExhibitEl.user_email = user_email;
+            myExhibitEl.nickname = nickname;
+          }
+        }
+      }
+      // likes x exhibition
+      for (let i = 0; i < likesData.length; i++) {
+        let likesEl = likesData[i].dataValues;
 
-  const authorData = [];
-  for (let i = 0; i < authors.length; i++) {
-    let el = {};
-    el.id = authors[i].id;
-    el.user_email = authors[i].user_email;
-    el.nickname = authors[i].nickname;
-    authorData.push(el);
+        for (let j = 0; j < myExhibition.length; j++) {
+          if (likesEl.exhibition_id === myExhibition[j].dataValues.id) {
+            const {
+              start_date,
+              end_date,
+              exhibit_desc,
+              exhibit_type,
+              genre_hashtags,
+              status,
+              title,
+              user_email,
+              nickname,
+            } = myExhibition[j].dataValues;
+            likesEl.start_date = start_date;
+            likesEl.end_data = end_date;
+            likesEl.exhibit_desc = exhibit_desc;
+            likesEl.exhibit_type = exhibit_type;
+            likesEl.genre_hashtags = genre_hashtags;
+            likesEl.status = status;
+            likesEl.title = title;
+            likesEl.user_email = user_email;
+            likesEl.nickname = nickname;
+          }
+        }
+        // likes x images
+        for (let k = 0; k < myExhibitionImgs.length; k++) {
+          if (
+            likesEl.exhibit_id === myExhibitionImgs[k].dataValues.exhibit_id
+          ) {
+            likesEl.image_urls = myExhibitionImgs[k].dataValues.image_urls;
+          }
+        }
+      }
+      res.status(200).json({
+        data: likesData,
+      });
+    }
+  } catch (error) {
+    console.log(error);
   }
-
-  // images
-  const myExhibitionImgs = await images.findAll({
-    where: {
-      exhibition_id: exhibition_ids,
-    },
-  });
-
-  const imgData = myExhibitionImgs.map((el) => el.dataValues.image_urls);
-  res.status(200).json({
-    imgData: imgData,
-    exhibitionData: myExhibition,
-    authorData: authorData,
-  });
 };
