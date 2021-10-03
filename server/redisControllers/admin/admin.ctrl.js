c1d8a72fe3973a132af6de2772b69ec44c5bab21
lee <xehddnr@naver.com>
2021-10-03 18:15:02 +0900

    feature/approveExhibitions

--- server/redisControllers/admin/admin.ctrl.js
+++ server/redisControllers/admin/admin.ctrl.js
@@ -6,22 +6,16 @@ const {
   caching,
   delCache,
 } = require("../../utils/redis/cache.ctrl");
+const axios = require("axios");
 module.exports = {
   getAllReviews: async (req, res) => {
     const userInfo = isAuthorized(req);
-    const redisKey = "allExhibition";
 
     if (userInfo.user_type === 3) {
-      const reply = await getCached(redisKey);
-      if (reply) {
-        const data = reply;
-        res.status(200).json({ data });
-      } else {
-        const result = await comments.findAll();
-        const data = result.map((el) => el.dataValues);
-        caching(redisKey, data);
-        res.status(200).json({ data });
-      }
+      const result = await comments.findAll();
+      const data = result.map((el) => el.dataValues);
+
+      res.status(200).json({ data });
     } else {
       res.status(401).json({
         message: "invalid access token",
@@ -29,17 +23,43 @@ module.exports = {
     }
   },
   approveExhibitions: async (req, res) => {
+    //전체 전시 정보에서는 status 수정 하고
+    //type 별 전시 정보에는 추가해야한다.
+
     const userInfo = isAuthorized(req);
-    const { postId, type: exhibit_type } = req.body;
-    const redisKey = "allExhibition";
-    const reply = await getCached(redisKey);
-    console.log("+++++++++++++++", req.headers);
-    console.log("userInfo:", userInfo, postId);
+    const { id, exhibit_type } = req.body.data;
+    const redisKey =
+      exhibit_type === undefined
+        ? "allExhibition"
+        : exhibit_type === "1"
+        ? "standard"
+        : "premium";
 
     if (userInfo.user_type === 3) {
+      const allExhibitionResult = await axios.get(
+        "https://art-ground.link/exhibition"
+      );
+      const typeExhibitionResult = await axios.get(
+        `https://art-ground.link/exhibition/${exhibit_type}`
+      );
+
+      const allExhibitionReply = allExhibitionResult.data.data;
+      const typeExhibitionReply = typeExhibitionResult.data.data;
+      allExhibitionReply.some((el) => {
+        if (el.id === id) {
+          el.status = 1;
+          return true;
+        }
+      });
+      typeExhibitionReply.push({ ...req.body.data });
+      caching("allExhibition", allExhibitionReply);
+      caching(redisKey, typeExhibitionReply);
+      res.status(200).json({
+        message: "exhibition successfully approved",
+      });
       const result = await exhibition.findOne({
         where: {
-          id: postId,
+          id,
         },
       });
 
@@ -48,7 +68,7 @@ module.exports = {
           message: "exhibition not exist",
         });
       } else {
-        const result = await exhibition.update(
+        await exhibition.update(
           {
             status: 1, // 전시 승인
           },
