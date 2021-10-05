6944d741c6041826e42baa44e76403b15691fb7e
lee <xehddnr@naver.com>
2021-10-05 23:52:49 +0900

    modify/script

--- scripts/initialize.sh
+++ scripts/initialize.sh
@@ -1,8 +1,8 @@
 
 
-sudo curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
-sudo unzip awscliv2.zip
-sudo ./aws/install
+# sudo curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+# sudo unzip awscliv2.zip
+# sudo ./aws/install
 
 
 #!/bin/bash
