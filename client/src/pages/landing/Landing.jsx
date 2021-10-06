f3dfc248c092e8c4da83d1be4067048c83388e58
devjade <jiyoung.park98@gmail.com>
2021-10-07 00:56:58 +0900

    feat/slider

--- client/src/pages/landing/Landing.jsx
+++ client/src/pages/landing/Landing.jsx
@@ -9,7 +9,6 @@ const Landing = () => {
   const wrapperRef = useRef(null);
   const loaderRef = useRef(null);
   const loaderRef2 = useRef(null);
-  //const boxRef = useRef(null);
   const contentRef = useRef(null);
  
   const txt = 'an artist + audience = "artience"';
@@ -80,13 +79,13 @@ const Landing = () => {
   //타이핑 효과 function
   useEffect(() => {
     const interval = setInterval(() => {
-      setText(Text + txt[Count]); // 이전 set한 문자 + 다음 문자
-      setCount(Count + 1); // 개수 만큼 체크 
+      setText(Text + txt[Count]); 
+      setCount(Count + 1); 
     }, 145);
-    if(Count === txt.length){  // Count를 따로 두지 않고 Text.length 체크도 가능
-        clearInterval(interval); // 문자열 체크를 통해 setInterval을 해제합니다
+    if(Count === txt.length){  
+        clearInterval(interval); 
     } 
-    return () => clearInterval(interval); // 언마운트시 setInterval을 해제합니다
+    return () => clearInterval(interval); 
   })
 
   const history = useHistory();
@@ -104,7 +103,6 @@ const Landing = () => {
       </div>
       <div>
         <div className={styles.container}>
-          {/* <div ref={boxRef} className={styles.box}></div> */}
           <div className={styles.imgWrapper}>
             <img className={styles.img} src="../images/video.gif" alt="landing video"/>
           </div>
