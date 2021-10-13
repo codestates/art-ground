8ca0c3e69e58b2f2ec3e7de03e42ce6e97068827
tkdkagody <tkdkagody@gmail.com>
2021-10-14 04:04:59 +0900

    edit:secretKey

--- client/src/pages/signup/SignUp.jsx
+++ client/src/pages/signup/SignUp.jsx
@@ -46,7 +46,8 @@ const SignUp = ({ isAudienceJoined }) => {
     return regExp.test(asValue);
   };
 
-  const secretKey = `${process.env.REACT_APP_CRYPTOJS_SECRETKEY}`;
+  //const secretKey = `${process.env.REACT_APP_CRYPTOJS_SECRETKEY}`;
+  const secretKey = "Klassiker";
 
   const clickAudJoin = () => {
     const { userEmail, nickname, password, password2 } = audInfo;
