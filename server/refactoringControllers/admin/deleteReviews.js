62a7a343912cc8f153b938c0f45bb0753d8b59ee
lee <xehddnr@naver.com>
2021-11-18 06:24:44 +0900

    Refactor deleteReviews

--- server/refactoringControllers/admin/deleteReviews.js
+++ server/refactoringControllers/admin/deleteReviews.js
@@ -1,16 +1,15 @@
-const { comments, exhibition, images, users } = require("../../models");
+const { comments } = require("../../models");
 const { isAuthorized } = require("../../utils/tokenFunction");
-const {} = require("../../utils/redis/ctrl/getCache.ctrl");
+const { removeHash } = require("../../utils/redis/ctrl/setCache.ctrl");
 module.exports = {
   deleteReviews: async (req, res) => {
     const userInfo = isAuthorized(req);
-
     const { postId, commentId } = req.params;
-    const exhibition_id = parseInt(postId);
     const id = parseInt(commentId);
 
     //comment:exhibition_id 받아온다.
     if (userInfo.user_type === 3) {
+      await removeHash(`comment:${id}`);
       res.status(200).json({
         message: "successfully delete comments",
       });
