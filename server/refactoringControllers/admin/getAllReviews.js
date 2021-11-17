ceb259ff71cc55f1f507f33731adaf0ce7008652
lee <xehddnr@naver.com>
2021-11-18 06:59:06 +0900

    Refactor getAllReviews

--- server/refactoringControllers/admin/getAllReviews.js
+++ server/refactoringControllers/admin/getAllReviews.js
@@ -1,8 +1,9 @@
-const { each, last, map, sortBy } = require("underscore");
+const { each, last, map, sortBy, isEqual } = require("underscore");
 const {
   getSet,
   getList,
   getHashValue,
+  getHash,
 } = require("../../utils/redis/ctrl/getCache.ctrl");
 const { isAuthorized } = require("../../utils/tokenFunction");
 
@@ -10,42 +11,44 @@ module.exports = {
   getAllReviews: async (req, res) => {
     const userInfo = isAuthorized(req);
 
-    //if (userInfo.user_type === 3) {
-    //nickname, title필요;
-    const allExhibitionId = await getSet("allExhibition");
+    if (userInfo.user_type === 3) {
+      const allExhibitionId = await getSet("allExhibition");
+      let data = [];
+      each(allExhibitionId, async (exhibitionId) => {
+        data.push(
+          ...(await Promise.all(
+            map(
+              await getSet(`exhibition:comment:${exhibitionId}`),
+              async (commentId) => {
+                const commentData = await getHash(`comment:${commentId}`);
+                commentData.exhibition = await getHashValue(
+                  `exhibition:${commentData.exhibition_id}`,
+                  "title"
+                );
+                commentData.user = await getHashValue(
+                  `user:${commentData.user_id}`,
+                  "id",
+                  "nickname",
+                  "profile_img"
+                );
+                return commentData;
+              }
+            )
+          ))
+        );
 
-    let data = [];
-    each(allExhibitionId, async (exhibitionId) => {
-      const commentr = await getList(`comment:${exhibitionId}`, 0, -1);
-      data.push(
-        ...(await Promise.all(
-          map(commentr, async (commentData) => {
-            commentData.exhibition = await getHashValue(
-              `exhibition:${commentData.exhibition_id}`,
-              "title"
-            );
-            commentData.user = await getHashValue(
-              `user:${commentData.user_id}`,
-              "id",
-              "nickname",
-              "profile_img"
-            );
-            return commentData;
-          })
-        ))
-      );
-      if (last(allExhibitionId) === exhibitionId) {
-        res.status(200).json({
-          data: sortBy(data, (el) => {
-            return Math.min(el.id);
-          }),
-        });
-      }
-    });
-    // } else {
-    //   res.status(401).json({
-    //     message: "invalid access token",
-    //   });
-    // }
+        if (isEqual(last(allExhibitionId), exhibitionId)) {
+          res.status(200).json({
+            data: sortBy(data, (el) => {
+              return Math.min(el.id);
+            }),
+          });
+        }
+      });
+    } else {
+      res.status(401).json({
+        message: "invalid access token",
+      });
+    }
   },
 };
