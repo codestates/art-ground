e370e017838a76b0bb3a2bf44c989fa65d4a6010
lee <xehddnr@naver.com>
2021-11-20 19:34:34 +0900

    Refactor: 중복 코드 제거

--- server/refactoringControllers/exhibition/likes.js
+++ server/refactoringControllers/exhibition/likes.js
@@ -1,44 +1,36 @@
 const { isAuthorized } = require("../../utils/tokenFunction");
-const { isInSet, getSet } = require("../../utils/redis/ctrl/getCache.ctrl");
+const { isInSet } = require("../../utils/redis/ctrl/getCache.ctrl");
 const { Op } = require("sequelize");
-const {
-  removeFromSet,
-  addToSet,
-} = require("../../utils/redis/ctrl/setCache.ctrl");
 const { likes } = require("../../models");
+const { addLikes, withdrawalLike } = require("../../utils/customFunction");
 
 module.exports.exhibitionLike = async (req, res) => {
-  // const userInfo = isAuthorized(req);
-  create;
-  // if (userInfo) {
-  removeFromSet;
-  // const { postId: exhibition_id } = req.body;
-  const userInfo = { id: 151 };
-  const exhibition_id = 91;
-  const { id: user_id } = userInfo;
+  const userInfo = isAuthorized(req);
 
-  const likesRedisKey = `like:${exhibition_id}`;
+  if (userInfo) {
+    const { postId: exhibition_id } = req.body;
+    const { id: user_id } = userInfo;
 
-  if (!(await isInSet(likesRedisKey, user_id.toString()))) {
-    addToSet(likesRedisKey, user_id);
-    res.status(201).json({
-      message: "successfully add like",
-    });
-    await likes.create({ exhibition_id, user_id });
+    if (!(await isInSet(`like:${exhibition_id}`, user_id.toString()))) {
+      await addLikes(exhibition_id, user_id);
+      res.status(201).json({
+        message: "successfully add like",
+      });
+      await likes.create({ exhibition_id, user_id });
+    } else {
+      await withdrawalLike(exhibition_id, user_id);
+      res.status(200).json({
+        message: "successfully delete like",
+      });
+      await likes.destroy({
+        where: {
+          [Op.and]: [{ exhibition_id }, { user_id }],
+        },
+      });
+    }
   } else {
-    removeFromSet(likesRedisKey, user_id);
-    res.status(200).json({
-      message: "successfully delete like",
-    });
-    await likes.destroy({
-      where: {
-        [Op.and]: [{ exhibition_id }, { user_id }],
-      },
+    res.status(401).json({
+      message: "invalid user",
     });
   }
-  // } else {
-  //   res.status(401).json({
-  //     message: "invalid user",
-  //   });
-  // }
 };
