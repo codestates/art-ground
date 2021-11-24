084490af1a2ddb315f85866b3c5bbbe0e7ffd7fc
lee <xehddnr@naver.com>
2021-11-24 10:58:58 +0900

    Refactor: 리뷰 삭제 기능

--- server/refactoringControllers/review/deleteReview.js
+++ server/refactoringControllers/review/deleteReview.js
@@ -1,50 +1,20 @@
 const { comments: commentsModel } = require("../../models");
 const { isAuthorized } = require("../../utils/tokenFunction");
-const {} = require("../../utils/redis/ctrl/getCache.ctrl");
+
+const {
+  removeFromSet,
+  removeHash,
+} = require("../../utils/redis/ctrl/setCache.ctrl");
 module.exports.deleteReview = async (req, res) => {
   const userInfo = isAuthorized(req);
 
-  const { postId, commentId } = req.params;
+  const { postId: exhibition_id, commentId } = req.params;
   if (userInfo) {
     const { id: user_id } = userInfo;
-    const exhibition_id = parseInt(postId);
-    const id = parseInt(commentId);
-    const redisKey = "exhibitionReview";
-    const reply = await getCached(redisKey);
 
-    if (reply) {
-      const reviewRedisKey = `${exhibition_id}Review`;
-      const DetailReview = await getCached(reviewRedisKey);
-
-      reply.some((el) => {
-        if (el.id === exhibition_id) {
-          el.comments.some((ele, idx) => {
-            if (ele.id === id) {
-              el.comments.splice(idx, 1);
-            }
-          });
-        }
-      });
-      DetailReview.commentsData.some((el, idx) => {
-        if (el.id === id) {
-          DetailReview.commentsData.splice(idx, 1);
-
-          return true;
-        }
-      });
-      caching(reviewRedisKey, DetailReview);
-      caching(redisKey, reply);
-      res.status(200).json({
-        message: "successfully delete comments",
-      });
-
-      await commentsModel.destroy({
-        where: {
-          id,
-          user_id,
-        },
-      });
-    }
+    await removeFromSet(`exhibition:comment:${exhibition_id}`, commentId);
+    await removeFromSet(`allExhibitionComment`, commentId);
+    await removeHash(`comment:${commentId}`);
   } else {
     res.status(401).json({
       message: "invalid user",
