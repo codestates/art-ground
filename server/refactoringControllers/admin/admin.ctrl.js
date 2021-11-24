c1297b6a0b6c97120aa94fced7b00327f496285e
Donguk Lee <52775482+dongukuklee@users.noreply.github.com>
2021-11-24 11:27:29 +0900

    Delete server/refactoringControllers directory

--- server/refactoringControllers/admin/admin.ctrl.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  getAllReviews: require("./getAllReviews").getAllReviews,
-  approveExhibitions: require("./approveExhibitions").approveExhibitions,
-  closeExhibitions: require("./closeExhibitions").closeExhibitions,
-  deleteReviews: require("./deleteReviews").deleteReviews,
-}; //
--- server/refactoringControllers/mypage/mypage.ctrl.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  getMyInfo: require("./getMyInfo").getMyInfo,
-  getMyExhibition: require("./exhibition").getMyExhibition,
-  getMylikes: require("./likes").getMyLikes,
-  modifyMyInfo: require("./modify").modifyMyInfo,
-  updatePassword: require("./updatePassword").updatePassword,
-  withdrawal: require("./withdrawal").withdrawal,
-}; //
