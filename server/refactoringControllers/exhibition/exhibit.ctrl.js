83d5dc682b8bda2f83b392177e4c16e2e1e635f7
lee <xehddnr@naver.com>
2021-11-28 15:22:04 +0900

    Refactor: 좋아요 데이터만 받아오는 getLikesCache 추가

--- server/refactoringControllers/exhibition/exhibit.ctrl.js
+++ server/refactoringControllers/exhibition/exhibit.ctrl.js
@@ -2,6 +2,6 @@ module.exports = {
   getExhibition: require("./getExhibition").getExhibition,
   exhibitionLike: require("./likes").exhibitionLike,
   register: require("./register").register,
-
+  getLikesCache: require("./getLikesCache").getLikesCache,
   getDetailExhibition: require("./getDetailExhibition").getDetailExhibition,
 }; //
